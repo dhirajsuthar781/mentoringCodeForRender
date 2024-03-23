819d7cf81db091d6b5ffbc66d3551353db8f892e
wjoe01090 <wjoe01090@gmail.com>
2024-03-23 19:27:27 +0530

    setting updated

--- backend/settings.py
+++ backend/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://mentoringcodeforrender.onrender.com"
 ]
 
 INSTALLED_APPS = [
